336b4e95fa4712b85a80d7ca66f3aa4ded6b14b6
Rampal Chopra <rampal.chopra@gmail.com>
2018-11-21 11:49:06 -0500

    update consul tests

--- testutil/server.go
+++ testutil/server.go
@@ -10,7 +10,11 @@ import (
 // MakeClientWithConfig will be written by Eric.
 func MakeClientWithConfig(t *testing.T) (*structs.Config, *testutil.TestServer) {
 
-	srv1 := testutil.NewTestServer(t)
+	srv1, err := testutil.NewTestServerConfigT(t, nil)
+	if err != nil {
+		t.Fatalf("Failed creating consul test server: %v", err)
+		return nil, nil
+	}
 
 	config := &structs.Config{
 		ConsulKeyRoot: "replicator/config",
